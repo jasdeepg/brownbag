ca2ee9eb789bcf309c34ec1be7b01a2f11c2189e
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2012-11-06 18:13:01 -0800

    base static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brownbag::Application.config.secret_token = '1a709292998744ac62de9345de8f7ce36f09e14f40129d6e5ae328550a39e559d6162b68cd46588f4dbaf4b11f01babae3f0be16fef47c6377767d494a7f2b81'
